a6b24d23cb697acb22642c06b00509449a82d27e
iann.braga <bragaprestes@gmail.com>
2024-01-25 18:36:49 -0300

    Add gem Devise / Estilização da página de login

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "dockerfile-rails", ">= 1.6", :group => :development
+
+gem "devise", "~> 4.9"
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,8 +6,9 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, "\\1en"
 #   inflect.singular /^(ox)en/i, "\\1"
-  inflect.irregular "evento", "eventos"
 #   inflect.uncountable %w( fish sheep )
+  inflect.irregular "evento", "eventos"
+  inflect.irregular "usuario", "usuarios"
 end
 
 # These inflection rules are supported but not enabled by default:
